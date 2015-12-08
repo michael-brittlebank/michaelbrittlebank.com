3cc0bd09549b681f3c5396348fb54c7a32dd84aa
mike-stumpf <mike.a.stumpf@gmail.com>
2015-12-07 19:32:29 -0500

    logger - updated logger to rotate log files

--- services/logger.service.js
+++ services/logger.service.js
@@ -1,12 +1,30 @@
 var winston = require('winston'),
+    dailyRotateFile = require('winston-daily-rotate-file'),
     logger = new (winston.Logger)({
         transports: [
-            new (winston.transports.Console)({ json: false, timestamp: true, level: 'debug' }),
-            new winston.transports.File({ filename: 'debug.log', json: false })
+            new (winston.transports.Console)({
+                json: false,
+                timestamp: true,
+                level: 'debug'
+            }),
+            new (dailyRotateFile)({
+                colorize: 'true',
+                filename: 'debug.log',
+                dirname: './logs',
+                datePattern: '.yyyy-MM-dd',
+                maxsize: 20000
+            })
         ],
         exceptionHandlers: [
-            new (winston.transports.Console)({ json: false, timestamp: true, level: 'debug' }),
-            new winston.transports.File({ filename: 'exceptions.log', json: false })
+            new (winston.transports.Console)({
+                json: false,
+                timestamp: true,
+                level: 'debug' }),
+            new winston.transports.File({
+                filename: 'exceptions.log',
+                dirname: './logs',
+                maxsize: 20000
+            })
         ],
         exitOnError: true
     });
