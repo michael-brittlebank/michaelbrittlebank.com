0d9019dc7d0bf40d6db0c6746c90c464711bf90c
mike-stumpf <mike.a.stumpf@gmail.com>
2015-12-09 21:08:49 -0500

    homepage - grid clicks now work

--- webapp/js/views/index.js
+++ webapp/js/views/index.js
@@ -1,21 +1,26 @@
 var homepage = {},
     squareBlocks,
+    bodyHighlightBlocks,
+    bodyHighlightBlocksText,
     activeBlock = null;
 
 homepage.grid = {
     activateBlock: function(){
-        $(activeBlock).find('.hover-filter-front').velocity({height: '50%'},{duration:500,queue: false});
+        $(activeBlock).find('.hover-filter-front').first().velocity({height: '50%'},{duration:500,queue: false});
+        $(activeBlock).find('.hover-filter-overlay').first().css({height: '100%'});
         setTimeout(function() {
-            $(activeBlock).find('.hover-filter-back').velocity({height: '50%',bottom:'50%'},{duration:500,queue: false});
+            $(activeBlock).find('.hover-filter-back').first().velocity({height: '50%',bottom:'50%'},{duration:500,queue: false});
             setTimeout(function() {
-                $(activeBlock).find('.hover-filter-text').velocity({height: '65px',opacity:'1'},{duration:500,queue: false});
+                $(activeBlock).find('.hover-filter-text').first().velocity({height: '65px',opacity:'1'},{duration:500,queue: false});
             }, 300);
         }, 350);
     },
     deactivateBlock: function(){
-        $(activeBlock).find('.hover-filter-back').velocity('stop', true).velocity({height: '0',bottom:'0'});
-        $(activeBlock).find('.hover-filter-text').velocity('stop', true).velocity({height: '0',opacity:'0'});
-        $(activeBlock).find('.hover-filter-front').velocity('stop', true).velocity({height: '0'});
+        $(activeBlock).find('.hover-filter-overlay').first().css({height: '0'});
+        $(activeBlock).find('.hover-filter-back').first().velocity('stop', true).velocity({height: '0',bottom:'0'});
+        $(activeBlock).find('.hover-filter-text').first().velocity('stop', true).velocity({height: '0',opacity:'0'});
+        $(activeBlock).find('.hover-filter-front').first().velocity('stop', true).velocity({height: '0'});
+        activeBlock = null;
     },
     makeSquare: function(){
         $(squareBlocks).css('height', $($(squareBlocks)[0]).css('width'));
@@ -24,8 +29,10 @@ homepage.grid = {
 
 homepage.init = function(){
     squareBlocks = $('.grid-block');
+    bodyHighlightBlocks = $(squareBlocks).filter('.body-highlight');
+    bodyHighlightBlocksText = $(bodyHighlightBlocks).find('.hover-filter-overlay');
     $(".expand-in").velocity("transition.expandIn");
-    $('.body-highlight').on('mouseover', function(){
+    $(bodyHighlightBlocks).on('mouseover touch click', function(){
             if (!activeBlock || ($(this).attr('id') && $(activeBlock).attr('id') !== $(this).attr('id'))){
                 if (activeBlock){
                     homepage.grid.deactivateBlock();
@@ -38,14 +45,17 @@ homepage.init = function(){
             if (activeBlock){
                 homepage.grid.deactivateBlock();
             }
-            activeBlock = null;
         });
-    $(window)
-        .load(function(){
+    $(bodyHighlightBlocksText).on('touch click',function(){
+        if(activeBlock){
+            window.location.href = $(activeBlock).data('url');
+        }
+    });
+    $(window).load(function(){
             homepage.grid.makeSquare();
         })
         .resize(function() {
             homepage.grid.makeSquare();
-    });
+        });
 };
 
