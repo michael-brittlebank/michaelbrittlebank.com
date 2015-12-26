6e3830666623e471a83c337409b427f405a5d080
mike-stumpf <mike.a.stumpf@gmail.com>
2015-12-26 07:44:07 -0600

    travel - more marker data

--- webapp/js/views/travel.js
+++ webapp/js/views/travel.js
@@ -27,29 +27,79 @@ travel.places = [
         lng:-91.5559
     },
     {
-        title:'',
-        lat:52.511,
-        lng:13.447
+        title:'Madison, Wisconsin, USA',
+        lat:43.0667,
+        lng:-89.4000
+    },
+    {
+        title:'Chicago, Illinois, USA',
+        lat:41.8369,
+        lng:-87.6847
+    },
+    {
+        title:'Charlotte, North Carolina, USA',
+        lat:35.2269,
+        lng:-80.8433
+    },
+    {
+        title:'Indianapolis, Indiana, USA',
+        lat:39.7910,
+        lng:-86.1480
+    },
+    {
+        title:'Baltimore, Maryland, USA',
+        lat:39.2833,
+        lng:-76.6167
+    },
+    {
+        title:'Washington D.C., USA',
+        lat: 38.9047,
+        lng:-77.0164
+    },
+    {
+        title:'St. Louis, Missouri, USA',
+        lat: 38.6272,
+        lng: -90.1978
+    },
+    {
+        title:'San Francisco, California, USA',
+        lat: 37.7833,
+        lng: -122.4167
+    },
+    {
+        title:'San Louis Obispo, California, USA',
+        lat: 35.2742,
+        lng: -120.6631
+    },
+    {
+        title:'Muir Woods National Monument, California, USA',
+        lat: 37.8919,
+        lng: -122.5708
+    },
+    {
+        title:'Portland, Oregon, USA',
+        lat: 45.5200,
+        lng: -122.6819
+    },
+    {
+        title:'Los Angeles, California, USA',
+        lat: 34.0500,
+        lng: 118.2500
+    },
+    {
+        title:'Milwaukee, Wisconsin, USA',
+        lat: 43.0500,
+        lng: 87.9500
     },
     {
         title:'',
-        lat:52.511,
-        lng:13.447
+        lat: 52.517,
+        lng: 13.394
     },
     {
         title:'',
-        lat:52.511,
-        lng:13.447
-    },
-    {
-        title:'',
-        lat:52.511,
-        lng:13.447
-    },
-    {
-        title:'',
-        lat:52.511,
-        lng:13.447
+        lat: 52.517,
+        lng: 13.394
     },
     //canada
     {
@@ -59,8 +109,24 @@ travel.places = [
     },
     {
         title:'Niagara Falls, Canada',
-        lat: 28.538336,
-        lng: -81.379236
+        lat: 43.1167,
+        lng: -79.0667
+    },
+    //mexico
+    {
+        title:'Costa Maya, Mexico',
+        lat: 18.7140,
+        lng: -87.7090
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
     },
 /**
  * asia
@@ -89,6 +155,22 @@ travel.places = [
 /**
  * europe
  */
+    //austria
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
     //england
     {
         title:'London, England',
@@ -116,6 +198,119 @@ travel.places = [
         lat: 51.507351,
         lng: -0.127758
     },
+    {
+        title:'Colmar, France',
+        lat: 48.0817,
+        lng: 7.3556
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    //germany
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    //hungary
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    //iceland
+    {
+        title:'Reykjavik, Iceland',
+        lat: 64.1333,
+        lng: 21.9333
+    },
+    {
+        title:'Thingvellir, Iceland',
+        lat: 64.2581,
+        lng: -21.1250
+    },
+    {
+        title:'Gullfoss, Iceland',
+        lat: 64.3261,
+        lng: -20.1211
+    },
+    {
+        title:'Geysir, Iceland',
+        lat: 64.3109,
+        lng: -20.3038
+    },
+    {
+        title:'Langjökull Glacier, Iceland',
+        lat: 64.7500,
+        lng: -19.9831
+    },
+    //italy
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    //ireland
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    //luxembourg
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    //monaco
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    //netherlands
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
     //norway
     {
         title:'Stavanger, Norway',
@@ -132,12 +327,66 @@ travel.places = [
         lat: 58.9861,
         lng: 6.1886
     },
+    //portugal
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    //slovakia
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    //slovenia
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
     //sweden
     {
         title:'Stockholm, Sweden',
         lat: 59.3294,
         lng: 18.0686
-    }
+    },
+    //switzerland
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
+    {
+        title:'',
+        lat: 52.517,
+        lng: 13.394
+    },
 ];
 
 travel.markers = [];
