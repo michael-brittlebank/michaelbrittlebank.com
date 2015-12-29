82a06f41c91829b85d60829e8592ab871b9c9686
mike-stumpf <mike.a.stumpf@gmail.com>
2015-12-29 12:53:23 -0600

    resume - updated loading animation to include stagger

--- webapp/js/views/resume.js
+++ webapp/js/views/resume.js
@@ -2,5 +2,6 @@ var resume = {};
 
 resume.init = function(){
     site.animation.fadeIn('#container-title',1000,500,0);
-    site.animation.fadeIn('#container-body',1000,1000,0);
+    site.animation.fadeIn('#container-body h2',1000,1000,200);
+    site.animation.fadeIn('#container-body p,#container-body h3',1000,1250,100);
 };
