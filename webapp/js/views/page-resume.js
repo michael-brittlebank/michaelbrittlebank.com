4b8d50e6b49ec27b33d0f8739a15d97abc08bd83
mike-stumpf <mike.a.stumpf@gmail.com>
2016-02-12 21:24:13 -0500

    resume - updated resume content and styling

--- webapp/js/views/page-resume.js
+++ webapp/js/views/page-resume.js
@@ -3,5 +3,5 @@ var resume = {};
 resume.init = function(){
     site.animation.fadeIn('#container-title',1000,500,0);
     site.animation.fadeIn('#container-body h2',1000,1000,200);
-    site.animation.fadeIn('#container-body p,#container-body ul,#container-body h3',1000,1250,100);
+    site.animation.fadeIn('#container-body p,#container-body ul,#container-body h3,#container-body h4,#container-body h5',1000,1250,100);
 };
