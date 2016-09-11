c1492893bcd1b0dd6692586ed976494539310634
mike-stumpf <mike.a.stumpf@gmail.com>
2016-09-11 12:10:22 +0100

    wordpress - themes - updated new theme to match old

--- wordpress/wp-content/themes/mstumpf/footer.php
+++ wordpress/wp-content/themes/mstumpf/footer.php
@@ -11,18 +11,16 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mstumpf' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mstumpf' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mstumpf' ), 'mstumpf', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info">
+
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-
+<script src="<?php echo get_template_directory_uri(); ?>/js/lib/iframeResizer.contentWindow.min.js"></script>
 </body>
 </html>
