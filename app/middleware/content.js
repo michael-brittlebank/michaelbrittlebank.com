3a14cc671caa41b862303f24190b3398f3ce7624
mike-stumpf <mike.a.stumpf@gmail.com>
2016-12-29 13:51:11 +0000

    header - added active classes to menu items

--- app/middleware/content.js
+++ app/middleware/content.js
@@ -1,4 +1,6 @@
-const //services
+const //packages
+    _ = require('lodash'),
+//services
     responseService = require('../services/response'),
     cacheService = require('../services/cache');
 
@@ -7,6 +9,23 @@ var contentMiddleware = {};
 contentMiddleware.getHeaderMenu = function(req,res,next){
     cacheService.getCachedHeaderMenu()
         .then(function(data){
+            var childIsActive;
+            _.filter(data, function(entry){
+                childIsActive = false;
+                if (!_.isEmpty(entry.children)){
+                    _.filter(entry.children, function(child){
+                        if(child.url === req.originalUrl){
+                            child.class = 'active';
+                            if(parseInt(child.parentId) !== 0){
+                                childIsActive = true;
+                            }
+                        }
+                    });
+                }
+                if(entry.url === req.originalUrl || childIsActive){
+                    entry.class = 'active';
+                }
+            });
             res.locals.menuItems = data;
             next();
         })
