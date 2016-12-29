4eb8cd7c32bff32ce03d5463b75c124f57a45c4c
mike-stumpf <mike.a.stumpf@gmail.com>
2016-12-29 21:58:16 +0000

    errors - added metadata to error pages

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -1,6 +1,8 @@
 const //packages
     _ = require('lodash'),
     promise = require('bluebird'),
+//models
+    contentModel = require('../models/content'),
 //services
     utilService = require('../services/util'),
     responseService = require('../services/response'),
@@ -9,7 +11,7 @@ const //packages
 
 var pages = {};
 
-/**
+/*
  main pages
  */
 pages.getIndex = function(req, res, next) {
@@ -69,33 +71,41 @@ pages.getTravelPage = function(req, res, next) {
         });
 };
 
-/**
+/*
  error pages
  */
 pages.get500Page = function(req, res, next) {
-    res.render('errors/500',{
-        meta: {
-            title: utilService.metaTitlePrefix+'500'
-        }
-    });
+    contentModel.getPageObject(JSON.stringify({title:'Error'}))
+        .then(function(response){
+            res.render('errors/500',{
+                page: response
+            });
+        })
+        .catch(function (error) {
+            responseService.defaultCatch(error, next,'500');
+        });
 };
 
 pages.get404Page = function(req, res, next) {
-    const translations = [
-        'Me paenitet',//latin
-        'I\'m sorry',//english
-        'Przepraszam',//polish
-        'Lo siento',//spanish
-        'Je m\'excuse',//french
-        'Mi dispiace',//italian
-        'Es tut mir Leid'//german
-    ];
-    res.render('errors/404', {
-        meta: {
-            title: utilService.metaTitlePrefix+'404'
-        },
-        translations: _.shuffle(translations)
-    });
+    contentModel.getPageObject(JSON.stringify({title:'Not Found',}))
+        .then(function(response){
+            const translations = [
+                'Me paenitet',//latin
+                'I\'m sorry',//english
+                'Przepraszam',//polish
+                'Lo siento',//spanish
+                'Je m\'excuse',//french
+                'Mi dispiace',//italian
+                'Es tut mir Leid'//german
+            ];
+            res.render('errors/404', {
+                page: response,
+                translations: _.shuffle(translations)
+            });
+        })
+        .catch(function (error) {
+            responseService.defaultCatch(error, next,'404');
+        });
 };
 
 module.exports = pages;
