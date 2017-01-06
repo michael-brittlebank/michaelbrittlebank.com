61ab3fbba00156ffa2103f805af532d3f917e821
mike-stumpf <mike.a.stumpf@gmail.com>
2017-01-06 23:08:40 +0000

    chorus - bug fix for init script

--- webapp/javascript/views/portfolio/chorus.js
+++ webapp/javascript/views/portfolio/chorus.js
@@ -5,39 +5,41 @@
 
     //functions
     this.init = function(){
+        if($('#portfolio-chorus').length > 0){
 
-        //functions
-        prettyPrint();
+            //functions
+            prettyPrint();
 
-        //chorus init
-        var guitarConfig = {
-                layoutInstrument: ['guitar'],
-                chorusDebug:'console'
-            },
-            pianoConfig = {
-                layoutInstrument: ['piano'],
-                chorusDebug:'console'
-            },
-            bassConfig = {
-                layoutInstrument: ['bass'],
-                chorusDebug:'console'
-            };
-        chorus.init('chorus-guitar', guitarConfig);
-        chorus.init('chorus-piano', pianoConfig);
-        chorus.init('chorus-bass', bassConfig);
+            //chorus init
+            var guitarConfig = {
+                    layoutInstrument: ['guitar'],
+                    chorusDebug:'console'
+                },
+                pianoConfig = {
+                    layoutInstrument: ['piano'],
+                    chorusDebug:'console'
+                },
+                bassConfig = {
+                    layoutInstrument: ['bass'],
+                    chorusDebug:'console'
+                };
+            chorus.init('chorus-guitar', guitarConfig);
+            chorus.init('chorus-piano', pianoConfig);
+            chorus.init('chorus-bass', bassConfig);
 
-        //listeners
-        $(document).on('chorusSearchComplete', function(e) {
-            setTimeout(function(){
-                chorus.populateLists('results-scales','results-chords');
-                //open modal
-                $('.results-container').each(function(){
-                    if(!$(this).is(":visible")){
-                        $('a[aria-controls='+$(this).attr('id')+']').click();
-                    }
-                });
-            },100);
-        });
+            //listeners
+            $(document).on('chorusSearchComplete', function(e) {
+                setTimeout(function(){
+                    chorus.populateLists('results-scales','results-chords');
+                    //open modal
+                    $('.results-container').each(function(){
+                        if(!$(this).is(":visible")){
+                            $('a[aria-controls='+$(this).attr('id')+']').click();
+                        }
+                    });
+                },100);
+            });
+        }
     };
 
 }).apply(app.views.portfolio.chorus);
