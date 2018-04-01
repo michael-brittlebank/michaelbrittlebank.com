94b39c5124e42800d55c1aa588874a2946e6adb0
mike-stumpf <mike.a.stumpf@gmail.com>
2018-04-01 14:57:54 +0100

    react upgrade - working on problem with jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.scss';
 
 const App = () => (
     <Router>
@@ -31,40 +32,22 @@ const Home = () => (
     </div>
 );
 
-const About = () => (
-    <div>
-        <h2>About</h2>
-    </div>
-);
+const logo = require('./logo.svg');
 
-const Topics = ({ match }) => (
-    <div>
-        <h2>Topics</h2>
-        <ul>
-            <li>
-                <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/components`}>Components</Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-            </li>
-        </ul>
-
-        <Route path={`${match.url}/:topicId`} component={Topic} />
-        <Route
-            exact
-            path={match.url}
-            render={() => <h3>Please select a topic.</h3>}
-        />
-    </div>
-);
-
-const Topic = ({ match }) => (
-    <div>
-        <h3>{match.params.topicId}</h3>
-    </div>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
