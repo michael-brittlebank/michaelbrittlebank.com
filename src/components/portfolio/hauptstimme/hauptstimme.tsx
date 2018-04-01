253fcc173dfaab425af40ccf27ef833f64f888d5
mike-stumpf <mike.a.stumpf@gmail.com>
2018-04-01 17:47:05 +0100

    hauptstimme - updated ingestion to match module exports

--- src/components/portfolio/hauptstimme/hauptstimme.tsx
+++ src/components/portfolio/hauptstimme/hauptstimme.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import * as hauptstimmeJs from '../../../../node_modules/hauptstimme-js/dist';
+import { HauptstimmeJs, NotesConstant } from '../../../../node_modules/hauptstimme-js/dist';
 import { Component } from 'react';
 
 export default class Hauptstimme extends Component {
     componentDidMount() {
         console.log('I was triggered during componentDidMount');
-        console.log('getting thing here', hauptstimmeJs.getLocalString(1));
+        console.log('getting thing here', HauptstimmeJs.getLocalString(1), NotesConstant.AB);
 
     }
 
     render() {
         console.log('I was triggered during render');
-        console.log('getting thing here', hauptstimmeJs.getLocalString(1));
+        console.log('getting thing here', HauptstimmeJs.getLocalString(1));
         return (
             <div>
                 <h1>Hauptstimme.JS</h1>
