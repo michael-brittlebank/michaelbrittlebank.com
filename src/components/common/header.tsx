e3b8e2e3b04f5b30eaf4d528aff610484a6f699f
mike-stumpf <mike.a.stumpf@gmail.com>
2018-04-17 22:30:28 -0500

    header - added active classes to menu links on mobile and standard menus

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -10,6 +10,7 @@ import * as faBook from '@fortawesome/fontawesome-free-solid/faBook'
 import * as faFile from '@fortawesome/fontawesome-free-solid/faFileAlt'
 import * as faTint from '@fortawesome/fontawesome-free-solid/faTint'
 import '../../sass/components/common/header.css'
+import * as classNames from 'classnames';
 
 export default class Header extends React.Component {
 
@@ -51,56 +52,73 @@ export default class Header extends React.Component {
         this.resumeMenuOption
     ];
 
+    private static isUrlActive(url: string | undefined): boolean {
+        if (!!url) {
+            if (url === '/') {
+                // home url
+                return window.location.pathname === '/';
+            } else {
+                // non-home url
+                return window.location.pathname.indexOf(url) !== -1;
+            }
+        } else {
+            // portfolio urls are special cases
+            return window.location.pathname.indexOf('portfolio') !== -1 || window.location.pathname.indexOf('hauptstimme') !== -1;
+        }
+    }
+
     render () {
         return (
             <header>
                 <div className="grid-container row">
+                    {/*mobile menu*/}
                     <div className="col-sm-12 show-for-small text-right position-relative">
-                        <div className="row">
-                            <div className="col-sm-2 text-center">
+                        <ul id="mobile-menu" className="row">
+                            <li className={classNames('col-sm-2 text-center', {'active': Header.isUrlActive(this.homeMenuOption.url)})}>
                                 {/*home*/}
                                 <Link to={this.homeMenuOption.url}>
                                     <FontAwesomeIcon icon={faHome} className="fa"/>
                                 </Link>
-                            </div>
-                            <div className="col-sm-2 text-center">
+                            </li>
+                            <li className={classNames('col-sm-2 text-center', {'active': Header.isUrlActive(this.haupstimmeMenuOption.url)})}>
                                 {/*hauptstimme*/}
                                 <Link to={this.haupstimmeMenuOption.url}>
                                     <FontAwesomeIcon icon={faMusic} className="fa"/>
                                 </Link>
-                            </div>
-                            <div className="col-sm-2 text-center">
+                            </li>
+                            <li className={classNames('col-sm-2 text-center', {'active': Header.isUrlActive(this.readingListMenuOption.url)})}>
                                 {/*reading list*/}
                                 <Link to={this.readingListMenuOption.url}>
                                     <FontAwesomeIcon icon={faBook} className="fa"/>
                                 </Link>
-                            </div>
-                            <div className="col-sm-2 text-center">
+                            </li>
+                            <li className={classNames('col-sm-2 text-center', {'active': Header.isUrlActive(this.bubbleMenuOption.url)})}>
                                 {/*bubble*/}
                                 <Link to={this.bubbleMenuOption.url}>
                                     <FontAwesomeIcon icon={faTint} className="fa"/>
                                 </Link>
-                            </div>
-                            <div className="col-sm-2 text-center">
+                            </li>
+                            <li className={classNames('col-sm-2 text-center', {'active': Header.isUrlActive(this.travelMenuOption.url)})}>
                                 {/*travel*/}
                                 <Link to={this.travelMenuOption.url}>
                                     <FontAwesomeIcon icon={faGlobe} className="fa"/>
                                 </Link>
-                            </div>
-                            <div className="col-sm-2 text-center">
+                            </li>
+                            <li className={classNames('col-sm-2 text-center', {'active': Header.isUrlActive(this.resumeMenuOption.url)})}>
                                 {/*resume*/}
                                 <Link to={this.resumeMenuOption.url}>
                                     <FontAwesomeIcon icon={faFile} className="fa"/>
                                 </Link>
-                            </div>
-                        </div>
+                            </li>
+                        </ul>
                     </div>
+                    {/*standard menu*/}
                     <nav className="col-sm-12 text-right hide-for-small">
                         <ul className="standard-menu">
                             {
                                 _.map(this.headerMenu, (menuOption: MenuOptionInterface, index: number) => {
                                     return (
-                                        <li key={index}>
+                                        <li key={index} className={classNames({'active': Header.isUrlActive(menuOption.url)})}>
                                             {
                                                 menuOption.url ?
                                                     <Link to={menuOption.url}>{menuOption.title}</Link> :
@@ -112,7 +130,7 @@ export default class Header extends React.Component {
                                                         {
                                                             _.map(menuOption.submenu, (innerMenuOption: MenuOptionInterface, innerIndex: number) => {
                                                                 return (
-                                                                    <li key={innerIndex}>
+                                                                    <li key={innerIndex} className={classNames({'active': Header.isUrlActive(innerMenuOption.url)})}>
                                                                         <Link to={innerMenuOption.url}>{innerMenuOption.title}</Link>
                                                                     </li>
                                                                 )
