0ab891087a192076b29b671a15c048985404f26e
mike-stumpf <mike.a.stumpf@gmail.com>
2018-04-29 13:09:37 -0500

    hauptstimme - added description and instructions

--- src/components/sections/hauptstimme.tsx
+++ src/components/sections/hauptstimme.tsx
@@ -48,24 +48,18 @@ export default class Hauptstimme extends React.Component<any, State> {
         return (
             <section id="hauptstimme-container" className="col-sm-12">
                 <h2 id="hauptstimme-title" className="section-header">Hauptstimme.js</h2>
-                <div>
-                    <h3>About</h3>
-                    <p>
-                        This project is registered as a <a href="http://npmjs.com/package/hauptstimme-js" target="_blank">npm package</a>.
-                    </p>
-                    <p>
-                        * There are <strong>90</strong> different fretted instrument tunings available as well as
-                        piano.
-                    </p>
-                    <p>
-                        * There are <strong>26</strong> searchable scale types resulting
-                        in <strong>300</strong> possible variations based on the root note.
-                    </p>
-                    <p>
-                        * There are <strong>45</strong> chord types which produce
-                        over <strong>600</strong> searchable results.
-                    </p>
-                </div>
+                <p>
+                    This project is for searching musical scales and chords which match the selected notes on the fretboard or keys. It is registered as a <a href="http://npmjs.com/package/hauptstimme-js" target="_blank">npm package</a> and built with <a href="https://www.typescriptlang.org/" target="_blank"> TypeScript</a>.
+                </p>
+                <p>
+                    There are <strong>91</strong> instrument tunings available and <strong>26</strong> scale and <strong>51</strong> chord types which produce <strong>312</strong> and <strong>612</strong> searchable variations, respectively.
+                </p>
+                <p>
+                    When using the interface, left click on any instrument fret or key to select a note. Your selection will be indicated by a blue background around the note you chose. Clicking a second time designates this note as the root note. This is illustrated by a red background. Right click on any selected note or root note to clear it. After you are finished selecting the combination of notes you want to search for, hit the search button.
+                </p>
+                <p>
+                    When the search has finished, the lists below the instrument will be populated with results that match the selected notes. Left click on any of these search results to apply it to the instrument above. Notes that are in the search result you selected but are not a user selected note will have a gold background. Click again on the same result to clear it from the instrument.
+                </p>
                 <div className="col-sm-12 row">
                     <div className="col-sm-12 col-md-6">
                         <h4>Instruments</h4>
@@ -125,90 +119,6 @@ export default class Hauptstimme extends React.Component<any, State> {
                     <span>View Code&nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faGithub} className="fa"/></span>
                 </a>
             </section>
-
-            // <aside className="modal--fade" id="modal-chorus-info" data-stackable="false" tabIndex="-1" role="dialog"
-            //        aria-labelledby="label-fade" aria-hidden="true">
-            //     <div className="modal-inner">
-            //         <div className="modal-content">
-            //             <h3 className="modal-title">Info &ndash; General</h3>
-            //             <p>
-            //                 * This project is registered as a <a href="http://bower.io/" target="_blank">Bower</a> package.
-            //             </p>
-            //             <pre className="prettyprint">bower install chorus-js --save</pre>
-            //             <br/>
-            //             <p>
-            //                 * This project was built entirely in native JavaScript and Sass.
-            //             </p>
-            //             <p>
-            //                 * This project is able to search both musical scales and chords to match the selected notes on
-            //                 the fretboard or piano keys.
-
-            //                 <h3 className="modal-title">Info &ndash; Notes</h3>
-            //                 <p>
-            //                     * Search results do not include theoretical scales, i.e. scales that contain notes with more
-            //                     than one sharp or flat.
-            //                 </p>
-            //                 <p>
-            //                     <a href="https://github.com/mike-stumpf/chorus.js" target="_blank"
-            //                        className="button portfolio-button-code">
-            //                         <span>View Code</span>
-            //                     </a>
-            //                 </p>
-            //         </div>
-            //     </div>
-            //     <a href="#!" className="modal-close" title="Close this modal"
-            //        data-dismiss="modal" data-close="Close">&times;</a>
-            // </aside>
-            // <aside className="modal--fade" id="modal-chorus-help" data-stackable="false" tabIndex="-1" role="dialog"
-            //        aria-labelledby="label-fade" aria-hidden="true">
-            //     <div className="modal-inner">
-            //         <div className="modal-content">
-            //             <h3 className="modal-title">Help &ndash; How Do I Use It?</h3>
-            //             <p>
-            //                 Left click on any instrument fret or piano key. Your selection will be indicated by a blue
-            //                 background around the note you chose.
-            //             </p>
-            //             <p className="chorus-note chorus-note-selected"><span>C&#9839;</span></p>
-            //             <p>
-            //                 Clicking a second time designates this note as the root note. This is illustrated by a red
-            //                 background.
-            //             </p>
-            //             <p className="chorus-note chorus-note-root"><span>F&#9839;</span></p>
-            //             <p>
-            //                 After you are finished selecting the combination of notes you want to search for, hit the search
-            //                 button.
-            //             </p>
-            //             <p className="text-center">
-            //                 <a className="chorus-search-button">Search Chorus</a>
-            //             </p>
-            //             <p>
-            //                 When the search has finished, the lists below the instrument will be populated with results that
-            //                 match the notes you selected.
-            //             </p>
-            //             <p>
-            //                 Left click on any of these search results to apply the selected result to the fretboard or piano
-            //                 keys above. Notes that are in the search result you selected but are not a previously selected
-            //                 or root note will have a gold background.
-            //             </p>
-            //             <p className="chorus-note chorus-note-found"><span>A</span></p>
-            //             <p>
-            //                 Click again on the same result to clear the result notes from the fretboard or piano keys. Click
-            //                 on another result to have the first result cleared and the second result populate the
-            //                 instrument's notes.
-            //             </p>
-            //             <h3 className="modal-title">Help &ndash; Notes</h3>
-            //             <p>
-            //                 * There can only be one root note per instrument.
-            //             </p>
-            //             <p>
-            //                 * Right click on a selected note or root note to clear its designation.
-            //             </p>
-            //         </div>
-            //     </div>
-            //     <a href="#!" className="modal-close" title="Close this modal"
-            //        data-dismiss="modal" data-close="Close">&times;</a>
-            // </aside>
-
         )
     }
 
