82d59e16bc8f7f7eca81bc7d65cac5edacaba19d
mike-stumpf <mike.a.stumpf@gmail.com>
2018-05-01 23:10:56 -0500

    travel - added quote to travel section

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -66,6 +66,15 @@ export class Home extends React.Component<Props, State> {
                         </div>
                     </div>
                     <div className="full-screen-grid-container row">
+                        <div id="travel-quote">
+                            <p>
+                                {String.fromCharCode(8220)}Go n-éirí an bóthar leat.{String.fromCharCode(8221)}
+                            </p>
+                            <p>
+                                {String.fromCharCode(8212)}
+                            </p>
+                            <p>May you have a sucessful journey.</p>
+                        </div>
                         <section id="travel-map-container" className="col-sm-12 no-padding">
                             <Map
                                 google={this.props.google}
