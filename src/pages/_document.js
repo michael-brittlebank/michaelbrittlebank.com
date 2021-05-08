1b8c5a813d1327f99a1503ea37ff5ad5c7a4651e
mike-stumpf <mike.a.stumpf@gmail.com>
2021-05-07 21:47:47 -0500

    minor fix for react camelcase naming convention in meta tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico"/>
           <meta charSet="utf-8"/>
           <meta name="theme-color" content="#000000"/>
-          <meta http-equiv="Content-Language" content="en"/>
+          <meta httpEquiv="Content-Language" content="en"/>
           <meta name="description" content="Personal website and portfolio for Mike Stumpf"/>
         </Head>
         <body>
