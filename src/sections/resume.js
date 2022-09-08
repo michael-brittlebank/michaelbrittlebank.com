316c13016195c87028dbb00b2a1c8d5d12fe2f7f
mike-stumpf <mike.a.stumpf@gmail.com>
2022-09-08 19:44:48 -0400

    updated resume section with new job title

--- src/sections/resume.js
+++ src/sections/resume.js
@@ -19,17 +19,20 @@ export default function Resume() {
           <section className="col-sm-12">
             <h2 className="section-header">Software Engineer</h2>
             <p>
-              Respected leader skilled in guiding and growing remote/distributed
-              cross-disciplinary teams. Thoroughly invested in professional
-              development, both technical and business breadth and depth, while
-              remaining a hands-on programmer.
+              Respected leader skilled in guiding remote/hybrid
+              cross-disciplinary teams. Lifelong mentor and teacher committed to
+              raising others up holistically. Thoroughly invested in a
+              continuous learning mindset focusing on business and technology,
+              breadth and depth.
             </p>
             <p>
               Effective communicator who is able to plan for and coordinate
               diverse deadlines while continuously managing expectations as well
-              as unexpected events. Doggedly focused on delivering iterative,
-              future-proof solutions which satisfy stakeholders across the
-              organization.
+              as unexpected events. Agile evangelist incorporating the best of
+              Lean, SAFe, Six Sigma, and other frameworks to meet deliverables
+              despite dependencies and roadblocks. Doggedly focused on
+              delivering iterative, future-proof solutions which satisfy
+              stakeholders across the organization.
             </p>
             <p>
               SOLID, DRY software engineer with a wide range of experience
@@ -41,7 +44,7 @@ export default function Resume() {
           </section>
           <section className="col-sm-12 col-md-6">
             <h2 className="section-header">Experience</h2>
-            <h3>Application Development Senior Engineer</h3>
+            <h3>Technical Solutions Delivery Manager</h3>
             <h5>
               2020&ndash;Present &mdash;{' '}
               <a href="https://www.amfam.com/" target="_blank" rel="noreferrer">
@@ -129,6 +132,34 @@ export default function Resume() {
                 University of Wisconsin-Madison. Madison, USA
               </a>
             </h5>
+            <h2 className="section-header">Certifications</h2>
+            <h4>
+              2022 &mdash;{' '}
+              <a
+                href="https://www.credential.net/cb532277-9544-4ef9-b757-6f129467351c"
+                target="_blank"
+                rel="noreferrer">
+                Google Cloud Certified – Associate Cloud Engineer
+              </a>
+            </h4>
+            <h4>
+              2021 &mdash;{' '}
+              <a
+                href="https://www.credly.com/badges/08765e92-c2fd-4eed-a08c-738cd59835e9"
+                target="_blank"
+                rel="noreferrer">
+                Leading SAFe, Certified SAFe 5 Agilist
+              </a>
+            </h4>
+            <h4>
+              2020 &mdash;{' '}
+              <a
+                href="https://www.credly.com/badges/1453a04d-da48-4d94-a3d0-1c642d144fee"
+                target="_blank"
+                rel="noreferrer">
+                AWS Certified Developer – Associate
+              </a>
+            </h4>
             <div id="resume-social-icons">
               <h3 id="resume-social-title">Find Me:</h3>
               <a
