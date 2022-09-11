a408fd60798858c02aa0e0055657763b5e708b43
mike-stumpf <mike.a.stumpf@gmail.com>
2022-09-11 17:53:36 -0400

    5.0.3

--- dist/apps/client/exported/_next/static/WQvmZwXRS7sgibji56z-o/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cb98f77df47d9f80.js"],"/_error":["static/chunks/pages/_error-e755336071b61fcc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
